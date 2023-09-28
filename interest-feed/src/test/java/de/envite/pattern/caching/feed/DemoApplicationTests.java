80b6ec632b4a8a557ef669646d39df429c5f41ca
Uwe Eisele <uwe.eisele@envite.de>
2023-09-28 13:15:49 +0200

    add gradle project

---
+++ interest-feed/src/test/java/de/envite/pattern/caching/feed/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.envite.pattern.caching.feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
