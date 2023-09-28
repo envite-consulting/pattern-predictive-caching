658ff95ab8308ad6699883b96573cff5a6a54e19
Nadja Hagen <nadja.hagen@envite.de>
2023-09-28 18:25:24 +0200

    Add datasource and SQL script

---
+++ news-db/init.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS newsdb;
+
+USE newsdb;
+
+CREATE TABLE IF NOT EXISTS news (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    link VARCHAR(1024),
+    headline VARCHAR(1024),
+    category VARCHAR(255),
+    short_description VARCHAR(2048),
+    authors VARCHAR(255),
+    date DATETIME
+);
+
+LOAD DATA INFILE '/etc/News_Category_Dataset_v3.csv' INTO TABLE news
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (link, headline, category, short_description, authors, @var1)
+    SET
+        id = NULL,
+        date = STR_TO_DATE(@var1, '%Y-%m-%d');
