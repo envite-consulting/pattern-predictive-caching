2f9860990b5e70f890451f6db4dac556c2aec853
Uwe Eisele <uwe.eisele@envite.de>
2023-10-02 12:52:00 +0200

    interest-feed: okhttp and http2 support for client

---
+++ interest-feed/src/main/java/de/envite/pattern/caching/feed/config/OkHttpClientProperties.java
@@ -0,0 +1,143 @@
+package de.envite.pattern.caching.feed.config;
+
+import okhttp3.Protocol;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties("okhttp.client")
+public class OkHttpClientProperties {
+
+    /**
+     * Defines if the OkHttp client should be used.
+     */
+    private boolean enabled = false;
+
+    /**
+     * Configure the protocols used by this client to communicate with remote servers.
+     *
+     * The following protocols are currently supported:
+     *
+     * * [http/1.1][rfc_2616]
+     * * [h2][rfc_7540]
+     * * [h2 with prior knowledge(cleartext only)][rfc_7540_34]
+     *
+     * If multiple protocols are specified, [ALPN][alpn] will be used to negotiate a transport.
+     * Protocol negotiation is only attempted for HTTPS URLs.
+     */
+    private List<Protocol> protocols = List.of(Protocol.HTTP_1_1);
+
+    /**
+     * Defines a time period in which our client should establish a connection with a target host.
+     */
+    private Duration connectTimeout = Duration.ofSeconds(10);
+    /**
+     * Defines a maximum time of inactivity between two data packets when waiting for the server’s response.
+     */
+    private Duration readTimeout = Duration.ofSeconds(10);
+    /**
+     * Defines a maximum time of inactivity between two data packets when sending the request to the server.
+     */
+    private Duration writeTimeout = Duration.ofSeconds(10);
+
+    /**
+     * Defines a time limit for a complete HTTP call.
+     * This includes resolving DNS, connecting, writing the request body, server processing, as well as reading the response body.
+     */
+    private Duration callTimeout = Duration.ZERO;
+
+    /**
+     * Manages reuse of HTTP and HTTP/2 connections for reduced network latency. HTTP requests that
+     * share the same [Address] may share a [Connection]. This class implements the policy
+     * of which connections to keep open for future use.
+     */
+    private Pool pool = new Pool();
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Protocol> getProtocols() {
+        return protocols;
+    }
+
+    public void setProtocols(List<Protocol> protocols) {
+        this.protocols = protocols;
+    }
+
+    public Duration getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Duration connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public Duration getReadTimeout() {
+        return readTimeout;
+    }
+
+    public void setReadTimeout(Duration readTimeout) {
+        this.readTimeout = readTimeout;
+    }
+
+    public Duration getWriteTimeout() {
+        return writeTimeout;
+    }
+
+    public void setWriteTimeout(Duration writeTimeout) {
+        this.writeTimeout = writeTimeout;
+    }
+
+    public Duration getCallTimeout() {
+        return callTimeout;
+    }
+
+    public void setCallTimeout(Duration callTimeout) {
+        this.callTimeout = callTimeout;
+    }
+
+    public Pool getPool() {
+        return pool;
+    }
+
+    public void setPool(Pool pool) {
+        this.pool = pool;
+    }
+
+    public static class Pool {
+
+        /**
+         * Defines the maximum number of idle connections id the pool.
+         */
+        private int maxIdleConnections = 5;
+        /**
+         * Defines the duration after which idle connections are evicted.
+         */
+        private Duration keepAliveDuration = Duration.ofMinutes(5);
+
+        public int getMaxIdleConnections() {
+            return maxIdleConnections;
+        }
+
+        public void setMaxIdleConnections(int maxIdleConnections) {
+            this.maxIdleConnections = maxIdleConnections;
+        }
+
+        public Duration getKeepAliveDuration() {
+            return keepAliveDuration;
+        }
+
+        public void setKeepAliveDuration(Duration keepAliveDuration) {
+            this.keepAliveDuration = keepAliveDuration;
+        }
+    }
+
+}
--- interest-feed/src/main/java/de/envite/pattern/caching/feed/config/RestTemplateConfiguration.java
+++ interest-feed/src/main/java/de/envite/pattern/caching/feed/config/RestTemplateConfiguration.java
@@ -1,9 +1,25 @@
 package de.envite.pattern.caching.feed.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.okhttp3.OkHttpConnectionPoolMetrics;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.stream.Collectors.toSet;
 
 @Configuration(proxyBeanMethods = false)
 public class RestTemplateConfiguration {
@@ -13,4 +29,36 @@ public class RestTemplateConfiguration {
         return restTemplateBuilder.build();
     }
 
+    @Bean
+    @ConditionalOnProperty(value = "okhttp.client.enabled", havingValue = "true")
+    public RestTemplateCustomizer httpRequestFactoryRestTemplateCustomizer(final ClientHttpRequestFactory httpRequestFactory) {
+        return restTemplate -> restTemplate.setRequestFactory(httpRequestFactory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory httpRequestFactory(final OkHttpClient httpClient) {
+        return new OkHttp3ClientHttpRequestFactory(httpClient);
+    }
+
+    @Bean
+    public OkHttpClient httpClient(final OkHttpClientProperties clientProperties, final MeterRegistry meterRegistry, final MetricsProperties properties) {
+        final ConnectionPool connectionPool = new ConnectionPool(clientProperties.getPool().getMaxIdleConnections(), clientProperties.getPool().getKeepAliveDuration().toMillis(), MILLISECONDS);
+        new OkHttpConnectionPoolMetrics(connectionPool, "okhttp.pool", toTags(properties.getTags())).bindTo(meterRegistry);
+        return new OkHttpClient.Builder()
+                .protocols(clientProperties.getProtocols())
+                .connectTimeout(clientProperties.getConnectTimeout())
+                .readTimeout(clientProperties.getReadTimeout())
+                .writeTimeout(clientProperties.getWriteTimeout())
+                .callTimeout(clientProperties.getCallTimeout())
+                .connectionPool(connectionPool)
+                .followRedirects(false)
+                .retryOnConnectionFailure(true)
+                .build();
+    }
+
+    private static Tags toTags(final Map<String, String> tags) {
+        return Tags.of(tags.entrySet().stream()
+                .map(e -> Tag.of(e.getKey(), e.getValue()))
+                .collect(toSet()));
+    }
 }
